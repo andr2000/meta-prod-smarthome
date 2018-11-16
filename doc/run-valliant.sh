7eee839afcd259c90336f9b791348cd841e1a0b6
Oleksandr Andrushchenko <andr2000@gmail.com>
2018-11-16 11:03:17 +0200

    valliant: Add QEMU run script
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

---
+++ doc/run-valliant.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -e
+JESSIE_KERNEL=kernel-qemu-4.4.34-jessie
+
+# This is the kernel that runs under QEMU + VersatilePB
+if [ ! -f ${JESSIE_KERNEL} ]; then
+    curl -OL https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-4.4.34-jessie
+fi
+
+
+usage()
+{
+    me=`basename "$0"`
+
+    echo "${me} -i|--initrd <initrd>"
+    echo "    initrd        path to cpio.gz initrd file"
+
+    if [ -n "$1" ]; then
+        echo "$1"
+    fi
+
+    exit 1
+}
+
+run_qemu()
+{
+    qemu-system-arm \
+        -M versatilepb \
+        -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/ram" \
+        -cpu arm1176 \
+        -kernel ${JESSIE_KERNEL} \
+        -initrd ${INITRD} \
+        -m 256M \
+        -nographic \
+        ;
+    }
+
+OPTIONS=i
+LONGOPTS=initrd:
+
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    usage
+    exit 1
+fi
+
+eval set -- "$PARSED"
+
+INITRD=""
+
+while true; do
+    case "$1" in
+        -i|--initrd)
+            INITRD="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+echo "Using initrd file: '$INITRD'"
+echo "Use Ctrl-A X to terminate QEMU"
+
+run_qemu
